3c2b6a70be47f298a8c818f4b0d4e11a7b919507
uCavalcante <ziel7@hotmail.com>
2020-05-29 00:10:07 -0300

    Initial commit

---
+++ test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace linked_list_test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            
+        }
+    }
+}
