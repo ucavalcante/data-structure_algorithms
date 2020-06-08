6fd931ef91f33bd1643087396b5e99f78efa0e08
uCavalcante <ziel7@hotmail.com>
2020-06-08 00:05:57 -0300

    add node

--- double-linked-list/src/library/Node.cs
+++ double-linked-list/src/library/Node.cs
@@ -1,7 +1,15 @@
+using System;
+
 namespace library
 {
     public class Node
     {
-        
+        public Object Value { get; set; }
+        public Node Next { get; set; }
+        public Node Previous { get; set; }
+        public Node(Object value)
+        {
+            this.Value = value;
+        }
     }
 }
