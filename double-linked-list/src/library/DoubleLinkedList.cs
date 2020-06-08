ecaf0922182e44334b4474359a3f2c62017968f9
uCavalcante <ziel7@hotmail.com>
2020-06-08 01:45:23 -0300

    implemented append method

--- double-linked-list/src/library/DoubleLinkedList.cs
+++ double-linked-list/src/library/DoubleLinkedList.cs
@@ -7,9 +7,24 @@ namespace double_linked_list
     {
         public Node Head { get; set; }
         public Node Tail { get; set; }
-        public void add(Object value)
+        public void Append(Object value)
         {
-
+            if (value is null)
+            {
+                throw new ArgumentNullException();
+            }
+            var newNode = new Node(value);
+            if (Head is null)
+            {
+                Head = newNode;
+                Tail = newNode;
+            }
+            else
+            {
+                newNode.Previous = Tail;
+                Tail.Next = newNode;
+                Tail = newNode;
+            }
         }
     }
 }
--- double-linked-list/test/DoubleLinkedListTest.cs
+++ double-linked-list/test/DoubleLinkedListTest.cs
@@ -16,5 +16,57 @@ namespace double_linked_list_test
             Assert.Null(variableX.Head);
             Assert.Null(variableX.Tail);
         }
+        [Fact]
+        public void AppendNullItemToDoubleLinkedListTest()
+        {
+            //Given
+            var variableX = new DoubleLinkedList();
+            Object variable1 = null;
+            //When
+            //Then
+            Assert.Throws<ArgumentNullException>(() =>
+            variableX.Append(variable1)
+            );
+        }
+        [Fact]
+        public void AppendOneItemToEmptyDoubleLinkedListTest()
+        {
+            //Given
+            var variableX = new DoubleLinkedList();
+            var variable1 = 1;
+            //When
+            variableX.Append(variable1);
+            //Then
+            Assert.Equal(variable1, variableX.Head.Value);
+            Assert.Equal(variable1, variableX.Tail.Value);
+            Assert.Null(variableX.Head.Next);
+            Assert.Null(variableX.Head.Previous);
+            Assert.Null(variableX.Tail.Next);
+            Assert.Null(variableX.Tail.Previous);
+        }
+        [Fact]
+        public void AppendMultipleItemToEmptyDoubleLinkedListTest()
+        {
+            //Given
+            var variableX = new DoubleLinkedList();
+            var variable1 = 1;
+            var variable2 = 2;
+            var variable3 = 3;
+            var variable4 = 4;
+            //When
+            variableX.Append(variable1);
+            variableX.Append(variable2);
+            variableX.Append(variable3);
+            variableX.Append(variable4);
+            //Then
+            Assert.Equal(variable1, variableX.Head.Value);
+            Assert.Equal(variable2, variableX.Head.Next.Value);
+            Assert.Equal(variable3, variableX.Head.Next.Next.Value);
+            Assert.Equal(variable4, variableX.Head.Next.Next.Next.Value);
+            Assert.Equal(variable4, variableX.Tail.Value);
+            Assert.Equal(variable3, variableX.Tail.Previous.Value);
+            Assert.Equal(variable2, variableX.Tail.Previous.Previous.Value);
+            Assert.Equal(variable1, variableX.Tail.Previous.Previous.Previous.Value);
+        }
     }
 }
